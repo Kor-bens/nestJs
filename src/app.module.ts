7b9a0cab3f07640e5abc5dce4a8d909a765aa29d
Tortosa <sofianetortosa@gmail.com>
2023-12-28 03:01:10 +0100

    hbs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import {ConfigModule} from "@nestjs/config"
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { MailerModule } from './mailer/mailer.module';
+import { AppController } from './app.controller';
 
 
 @Module({
+  controllers: [AppController],
   imports: [ ConfigModule.forRoot({isGlobal : true}), AuthModule, PrismaModule, MailerModule],
 })
 export class AppModule {}
