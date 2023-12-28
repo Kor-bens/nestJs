6abd3c53af615d5cbef137b88c93ff9009a46528
Tortosa <sofianetortosa@gmail.com>
2023-12-28 03:17:45 +0100

    hbs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,13 @@ export class AppController {
   @Get()
   @Render('index')
   root() {
-    return { message: 'Hello world!' };
+    return { message: 'Hello worldaaaa!',
+             navbar: ` <nav>
+             <ul>
+               <li><a href="#">Accueil</a></li>
+               <li><a href="#">Menu</a></li>
+               <li><a href="#">Photos</a></li>
+             </ul>
+           </nav>` };
   }
 }
